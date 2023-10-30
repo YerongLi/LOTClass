3dc28b61e53b7914c8f0d8510a2a4867514c5cf6
Yerong <yerong.li@outlook.com>
2023-10-29 23:42:59 -0500

    Test lla

--- llama_movies.py
+++ llama_movies.py
@@ -32,7 +32,7 @@ def todevice(feat, device):
 logging.info(f'Logger start: {os.uname()[1]}')
 # model_path = "/scratch/yerong/.cache/pyllama/vicuna-7b-v1.3"
 # model_path = "/scratch/yerong/.cache/pyllama/Llama-2-7b-chat-hf/"
-model_path, model_name = "vicuna-7b-v1.3", "vc"
+model_path, model_name = "/scratch/yerong/.cache/pyllama/vicuna-7b-v1.3", "vc"
 print(model_name)
 # model_path, model_name = "/scratch/yerong/.cache/pyllama/Llama-2-7b-hf/", 'llama'
 # device = "cuda:0"  # You can set this to "cpu" if you don't have a GPU
