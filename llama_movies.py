f7a4b228e32a8b50dcd2401e106291c08dd98d96
Yerong <yerong.li@outlook.com>
2023-10-29 22:27:21 -0500

    Upload pylaama

--- llama_movies.py
+++ llama_movies.py
@@ -1,4 +1,48 @@
 import os
+from math import ceil
+import logging
+import torch
+import os
+import pickle
+from transformers import AutoModelForCausalLM, AutoTokenizer,LlamaForCausalLM
+from modeling_llama_partial import LlamaForCausalLMPartial
+
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-4s - %(filename)-6s:%(lineno)d - %(message)s',
+    level=logging.INFO,
+    filename='./output.log',
+    datefmt='%m-%d %H:%M:%S',
+    force=True)
+def todevice(feat, device):
+    # Initialize an empty list to store the modified tensors
+    modified_feat = []
+
+    # Iterate over items in feat
+    for item in feat:
+        if isinstance(item, torch.Tensor):
+            # If the item is a torch tensor, cast it to the specified device
+            modified_item = item.to(device)
+            modified_feat.append(modified_item)
+        else:
+            # If the item is not a tensor, leave it as it is
+            modified_feat.append(item)
+
+    # Return the modified feat
+    return tuple(modified_feat)
+logging.info(f'Logger start: {os.uname()[1]}')
+# model_path = "/scratch/yerong/.cache/pyllama/vicuna-7b-v1.3"
+# model_path = "/scratch/yerong/.cache/pyllama/Llama-2-7b-chat-hf/"
+model_path = "/scratch/yerong/.cache/pyllama/Llama-2-7b-chat-hf/"
+device = "cuda:0"  # You can set this to "cpu" if you don't have a GPU
+
+# Load the model
+logging.info('Loading the model')
+model = LlamaForCausalLMPartial.from_pretrained(
+    model_path,
+    torch_dtype=torch.float16,
+    trust_remote_code=True,
+    device_map='auto',
+).eval()
 text_file='movies_train.txt'
 dataset_dir='datasets/movies'
 loader_file='llama.pt'
