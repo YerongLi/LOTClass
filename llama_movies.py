47f8a30a76600582ea354c09e81206c73df29169
Yerong <yerong.li@outlook.com>
2023-10-29 23:44:03 -0500

    Test lla

--- llama_movies.py
+++ llama_movies.py
@@ -40,18 +40,18 @@ device='cuda:0'
 
 # Load the model
 logging.info('Loading the model')
-# model = AutoModelForCausalLM.from_pretrained(
-#     model_path,
-#     torch_dtype=torch.float16,
-#     trust_remote_code=True,
-#     device_map='auto',
-# ).eval()
-
 model = AutoModelForCausalLM.from_pretrained(
     model_path,
     torch_dtype=torch.float16,
     trust_remote_code=True,
-).to(device).eval()
+    device_map='auto',
+).eval()
+
+# model = AutoModelForCausalLM.from_pretrained(
+#     model_path,
+#     torch_dtype=torch.float16,
+#     trust_remote_code=True,
+# ).to(device).eval()
 tokenizer = AutoTokenizer.from_pretrained(model_path)
 predicted_labels = []
 predicted_tokens = []
