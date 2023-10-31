e337ca685691101a8837e53ee7782803eb5029ef
Yerong <yerong.li@outlook.com>
2023-10-30 21:58:01 -0500

    Update

--- eval1.py
+++ eval1.py
@@ -18,6 +18,29 @@ with open(f'datasets/{dataset}/{dataset}_train_labels.txt', 'r') as file:
 args = parse_args()
 
 if args.vote:
+    filenames = ['out.txt', 'vc_out.txt', 'llama_out.txt']
+    predicted_labels = []
+
+    for filename in filenames:
+        with open(f'datasets/{dataset}/{filename}', 'r') as file:
+            labels = [int(line.strip()) for line in file]
+            predicted_labels.append(labels[:100])  # Considering 100 labels from each file for voting
+
+    # Voting among the predicted labels
+    voted_predictions = [max(set(prediction), key=prediction.count) for prediction in zip(*predicted_labels)]
+
+    # Calculate precision, recall, and F1 score based on voting
+    precision = precision_score(true_labels[:100], voted_predictions, average='macro')
+    recall = recall_score(true_labels[:100], voted_predictions, average='macro')
+    f1 = f1_score(true_labels[:100], voted_predictions, average='macro')
+
+    print("Metrics based on voting:")
+    print(f'Precision: {precision:.4f}')
+    print(f'Recall: {recall:.4f}')
+    print(f'F1 Score: {f1:.4f}')
+
+else:
+    model_filenames = {'vc': 'vc_out.txt', 'llama': 'llama_out.txt', 'lot': 'out.txt'}
     model_file = model_filenames[args.model]
 
     # Read the predicted labels based on the chosen model
@@ -30,23 +53,6 @@ if args.vote:
     f1 = f1_score(true_labels[:100], predicted_labels, average='macro')
 
     print(f"Metrics for {model_file}:")
-    print(f'Precision: {precision:.4f}')
-    print(f'Recall: {recall:.4f}')
-    print(f'F1 Score: {f1:.4f}')
-
-else:
-    # Default evaluation using 'lot' as the model
-    model_result = {'vc': 'vc_out.txt', 'llama': 'llama_out.txt', 'lot': 'out.txt'}
-
-    with open(f'datasets/{dataset}/{model_result}', 'r') as file:
-        predicted_labels = [int(line.strip()) for line in file][:100]
-
-    # Calculate precision, recall, and F1 score for the default model
-    precision = precision_score(true_labels[:100], predicted_labels, average='macro')
-    recall = recall_score(true_labels[:100], predicted_labels, average='macro')
-    f1 = f1_score(true_labels[:100], predicted_labels, average='macro')
-
-    print("Metrics for out.txt (default 'lot' model):")
     print(f'Precision: {precision:.4f}')
     print(f'Recall: {recall:.4f}')
     print(f'F1 Score: {f1:.4f}')
